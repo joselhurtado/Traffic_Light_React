8fba9643b902e302e2e055fd7aa08936c57ac10e
Jose Hurtado <hello@josehurtado.net>
2021-01-11 23:56:39 +0000

    Home Changes

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,13 +7,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 export function Home() {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
+			<h1>Traffic Light React Excercise</h1>
 			<p>
 				Made by{" "}
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
